6150427e4975cd68d4641409ae8b15b7b5c9ee5a
rebecca_h2202 <s2091339@ed.ac.uk>
2023-09-25 11:58:47 +0100

    filtering product availabiity by european lakes + visualising

---
+++ filter by european lakes.R
@@ -0,0 +1,60 @@
+lakes <- read.csv("data/lakes_cci_v2.0.2_product_availability.csv")
+
+lakes4 <- filter(lakes, lat_min_box > 35, lat_max_box < 72, lon_min_box > -10, lon_max_box < 65)
+lakes4 <- filter(lakes4, !country == "Algeria")
+lakes4 <- filter(lakes4, !country == "Tunisia")
+lakes4 <- filter(lakes4, !country == "Uzbekistan")
+lakes4 <- filter(lakes4, !country == "Kazakhstan")
+lakes4 <- filter(lakes4, !country == "Turkmenistan", !country == "Uzbekistan;Kazakhstan", !country == "Turkmenistan;Uzbekistan", !country == "Azerbaijan",
+                 !country == "Iran Islamic Republic of;Azerbaijan", !country == "Iraq", !country == "Syrian Arab Republic")
+
+
+library(tidyverse)
+# install.packages("maps")
+library(eurostat)
+library(leaflet)
+library(sf)
+library(scales)
+library(cowplot)
+library(ggthemes)
+
+SHP_0 <- get_eurostat_geospatial(resolution = 10, 
+                                 nuts_level = 0, 
+                                 year = 2016)
+
+SHP_0 %>% 
+  ggplot() +
+  geom_sf() +
+  scale_x_continuous(limits = c(-10, 35)) +
+  scale_y_continuous(limits = c(35, 72)) +
+  theme_classic()
+
+ggplot(data=lakes4, aes(x=lon_max_box, y = lat_max_box))+
+  geom_point() +
+  theme_classic()
+
+library(ggplot2)
+library(maps)  
+worldmap = map_data('world')
+
+ggplot() + geom_polygon(data = worldmap, 
+                        aes(x = long, 
+                            y = lat, 
+                            group = group)) + 
+  coord_fixed(xlim = c(-10,3), 
+              ylim = c(50.3, 59))
+
+ggplot() + 
+  geom_polygon(data = worldmap, 
+               aes(x = long, y = lat, group = group), 
+               fill = 'gray90', color = 'black') + 
+  coord_fixed(ratio = 1.3, xlim = c(-10,65), ylim = c(35, 72)) + 
+  theme_classic() + 
+  geom_point(data = lakes4, 
+             aes(x = as.numeric(lon_min_box), 
+                 y = as.numeric(lat_min_box), alpha = .7), colour = "blue") + 
+  scale_size_area(max_size = 8) + 
+  scale_color_viridis_c() + 
+  theme(legend.position = 'none') + 
+  theme(title = element_text(size = 12))
+
